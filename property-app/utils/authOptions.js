c0d785e506696bd02b958ab8c16dee4f1687ff9d
Ilie Vlad <32937795+vlad434@users.noreply.github.com>
2024-10-16 17:47:50 +0300

    Made some pages unaccesible when user is not logged in

--- property-app/utils/authOptions.js
+++ property-app/utils/authOptions.js
@@ -3,6 +3,7 @@ import User from "@/models/User";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
+  secret: process.env.NEXTAUTH_SECRET, // Adaugă această linie
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -17,34 +18,22 @@ export const authOptions = {
     }),
   ],
   callbacks: {
-    // Invoked on successful sigin
     async signIn({ profile }) {
-      //1. Connect to db
       await connectDB();
-      //2. Check if user exists
       const userExists = await User.findOne({ email: profile.email });
-      //3. If not, then add user to db
       if (!userExists) {
-        //Truncate user name if too long
         const username = profile.name.slice(0, 20);
-
         await User.create({
           email: profile.email,
           username,
           image: profile.picture,
         });
       }
-      //4. return true to allow sign in
       return true;
     },
-
-    //Modifies the session object
     async session({ session }) {
-      //1. Get user from db
       const user = await User.findOne({ email: session.user.email });
-      //2. Assign the user id to the session
       session.user.id = user._id.toString();
-      //3. Return session
       return session;
     },
   },
